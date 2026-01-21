8cd69a0ba766e4e58757485e856c1269092867c9
Carel Prianugra Ceri Pratama <carel.prianugra.cp@gmail.com>
2026-01-21 08:25:10 +0700

    rename a file

---
+++ app/Http/Controllers/MandiriController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Mandiri;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\MandirisImport;
+
+class MandiriController extends Controller
+{
+    public function index(){
+        $mandiris = Mandiri::orderBy('created_at', 'asc')->get();
+
+        return view('mandiris.index', ['mandiris' => $mandiris]);
+    }
+
+    public function create(){
+        return view('mandiris.create');
+    }
+
+    public function store(Request $request){
+        $data = $request->validate([
+            'uker' => 'nullable|string|max:255'
+            // 'cabang_bank' => 'nullable|string|max:255',
+            // 'nama_debitur' => 'nullable|string|max:255',
+            // 'nomor_rekening' => 'nullable|string|max:255',
+            // 'nilai_tuntutan_klaim' => 'nullable|string|max:255',
+
+            // 'tanggal_klaim_diterima' => 'nullable|date',
+            // 'tanggal_klaim_masuk_portal' => 'nullable|date',
+            // 'date_update' => 'nullable|date',
+
+            // 'status' => 'nullable|string|max:50',
+            // 'tambahan_data' => 'nullable|string|max:255',
+            // 'nomor_box' => 'nullable|string|max:255',
+        ]);
+
+
+        // $data['outstanding'] = str_replace('.', '', $data['outstanding']);
+        // $data['outstanding'] = str_replace(',', '.', $data['outstanding']);
+        // $data['outstanding'] = (float)$data['outstanding'];
+
+        // if (strpos($data['tanggal_polis'], '/') !== false) {
+        //     $data['tanggal_polis'] = \Carbon\Carbon::createFromFormat('d/m/Y', $data['tanggal_polis'])->format('Y-m-d');
+        // }
+        // if (strpos($data['wpc'], '/') !== false) {
+        //     $data['wpc'] = \Carbon\Carbon::createFromFormat('d/m/Y', $data['wpc'])->format('Y-m-d');
+        // }
+
+        Mandiri::create($data);
+
+        // return redirect(route('dashboard'))->with('success', 'Klaim baru berhasil ditambahkan!');
+        return redirect()->route('dashboard', ['table' => 'mandiri'])->with('success', 'Klaim baru berhasil ditambahkan!');
+    }
+
+    public function edit(Mandiri $mandiris){
+        return view('mandiris.edit', ['mandiris' => $mandiris]);
+    }
+
+    public function update(Mandiri $mandiris, Request $request){
+        $data = $request->validate([
+            'uker' => 'nullable|string|max:255'
+            // 'cabang_bank' => 'nullable|string|max:255',
+            // 'nama_debitur' => 'nullable|string|max:255',
+            // 'nomor_rekening' => 'nullable|string|max:255',
+            // 'nilai_tuntutan_klaim' => 'nullable|string|max:255',
+
+            // 'tanggal_klaim_diterima' => 'nullable|date',
+            // 'tanggal_klaim_masuk_portal' => 'nullable|date',
+            // 'date_update' => 'nullable|date',
+
+            // 'status' => 'nullable|string|max:50',
+            // 'tambahan_data' => 'nullable|string|max:255',
+            // 'nomor_box' => 'nullable|string|max:255',
+        ]);
+
+        // $data['outstanding'] = str_replace('.', '', $data['outstanding']);
+        // $data['outstanding'] = str_replace(',', '.', $data['outstanding']);
+        // $data['outstanding'] = (float)$data['outstanding'];
+
+        // if (strpos($data['tanggal_polis'], '/') !== false) {
+        //     $data['tanggal_polis'] = \Carbon\Carbon::createFromFormat('d/m/Y', $data['tanggal_polis'])->format('Y-m-d');
+        // }
+        // if (strpos($data['wpc'], '/') !== false) {
+        //     $data['wpc'] = \Carbon\Carbon::createFromFormat('d/m/Y', $data['wpc'])->format('Y-m-d');
+        // }
+
+        $mandiris->update($data);
+
+        return back()->with('success', 'Klaim berhasil di-update!');
+    }
+
+    public function destroy(Mandiri $mandiris){
+        $mandiris->delete();
+        return redirect(route('dashboard'))->with('success', 'Klaim berhasil dihapus!');
+    }
+
+    public function search(Request $request){
+        $keyword = $request->query('q', '');
+
+        $data = Mandiri::where('uker', 'like', "%$keyword%")
+        // ->orWhere('cabang_bank', 'like', "%$keyword%")
+        // ->orWhere('nama_debitur', 'like', "%$keyword%")
+        // ->orWhere('nomor_rekening', 'like', "%$keyword%")
+        // ->orWhere('nilai_tuntutan_klaim', 'like', "%$keyword%")
+        // ->orWhere('tanggal_klaim_diterima', 'like', "%$keyword%")
+        // ->orWhere('tanggal_klaim_masuk_portal', 'like', "%$keyword%")
+        // ->orWhere('status', 'like', "%$keyword%")
+        // ->orWhere('tambahan_data', 'like', "%$keyword%")
+        // ->orWhere('date_update', 'like', "%$keyword%")
+        // ->orWhere('nomor_box', 'like', "%$keyword%")
+        ->get();
+
+        return view('components.mandiri-table', [
+            'mandiris' => $data
+        ]);
+    }
+
+    public function upload(Request $request){
+        $request->validate([
+            'file' => 'required|file|mimes:xls,xlsx'
+        ]);
+
+        Excel::import(new MandirisImport, $request->file('file'));
+        
+        return redirect()->route('dashboard')->with('success', 'Excel berhasil di-import!');
+    }
+}
---
+++ app/Models/Mandiri.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Symfony\Component\Mime\Email;
+
+class Mandiri extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'uker'
+        // 'cabang_bank',
+        // 'nama_debitur',
+        // 'nomor_rekening',
+        // 'nilai_tuntutan_klaim',
+        // 'tanggal_klaim_diterima',
+        // 'tanggal_klaim_masuk_portal',
+        // 'status',
+        // 'tambahan_data',
+        // 'date_update',
+        // 'nomor_box'
+    ];
+}
