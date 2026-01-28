6a0f62f1a8d423fd9579ac7e1dee9bffd0e2921d
Carel Prianugra Ceri Pratama <carel.prianugra.cp@gmail.com>
2026-01-28 09:17:03 +0700

    do a small fix on the bankjatim

--- resources/views/components/bankjatim-table.blade.php
+++ resources/views/components/bankjatim-table.blade.php
@@ -44,16 +44,7 @@
                     <td class="text-center">{{ $index + 1 }}</td>
                     <td class="text-center">{{ $bankjatim->cabang_bank }}</td>
                     <td class="text-center">{{ $bankjatim->nama }}</td>
-                    <!-- <td class="text-center">{{ $mandiri->nama_debitur }}</td>
-                    <td class="text-center">{{ $mandiri->nomor_rekening }}</td>
-                    <td class="text-center">{{ $mandiri->tuntutan }}</td>
-                    <td class="text-center">{{ $mandiri->net_klaim }}</td>
-                    <td class="text-center">{{ $mandiri->tanggal_klaim_diajukan }}</td>
-                    <td class="text-center">{{ $mandiri->status }}</td>
-                    <td class="text-center">{{ $mandiri->keterangan }}</td>
-                    <td class="text-center">{{ $mandiri->kekurangan_data }}</td>
-                    <td class="text-center">{{ $mandiri->tanggal_update }}</td>
-                    <td class="text-center">{{ $mandiri->nomor_box }}</td> -->
+                
 
                     <td class="text-center">
                         <div class="d-inline-flex gap-2">
@@ -97,50 +88,6 @@
                 <div class="modal-body">
                     <div class="mb-3"><label class="form-label">Nama</label><input name="nama" class="form-control" value="{{ $bankjatim->nama }}"></div>
                 </div>
-
-                <!-- <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Nama Debitur</label><input name="nama_debitur" class="form-control" value="{{ $mandiri->nama_debitur }}"></div>
-                </div>
-
-                <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Nomor Rekening</label><input name="nomor_rekening" class="form-control" value="{{ $mandiri->nomor_rekening }}"></div>
-                </div>
-
-                <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Tuntutan</label><input name="tuntutan" class="form-control" value="{{ $mandiri->tuntutan }}"></div>
-                </div>
-
-                <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Net Klaim</label><input name="net_klaim" class="form-control" value="{{ $mandiri->net_klaim }}"></div>
-                </div>
-
-                <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Tanggal Klaim Diajukan</label><input name="tanggal_klaim_diajukan" class="form-control" value="{{ $mandiri->tanggal_klaim_diajukan }}"></div>
-                </div>
-
-                <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Status</label><input name="status" class="form-control" value="{{ $mandiri->status }}"></div>
-                </div>
-                
-                <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Keterangan</label><input name="keterangan" class="form-control" value="{{ $mandiri->keterangan }}"></div>
-                </div>
-
-                <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Kekurangan Data</label><input name="kekurangan_data" class="form-control" value="{{ $mandiri->kekurangan_data }}"></div>
-                </div>
-
-                <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Tanggal Update</label><input name="tanggal_update" class="form-control" value="{{ $mandiri->tanggal_klaim_diajukan }}"></div>
-                </div>
-
-                <div class="modal-body">
-                    <div class="mb-3"><label class="form-label">Nomor Box</label><input name="nomor_box" class="form-control" value="{{ $mandiri->nomor_box }}"></div>
-                </div>
-
-                <div class="modal-footer justify-content-center">
-                    <button class="btn btn-primary">Update</button>
-                </div> -->
             </form>
         </div>
     </div>
